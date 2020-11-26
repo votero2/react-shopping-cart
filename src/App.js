c232b3a7fc6fe006661a9890e563eec7b5037f4d
votero2 <votero2@yahoo.es>
2020-11-26 15:22:13 -0500

    cart-component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //feature 1
 import React from 'react';
+import Cart from './components/Cart';
 import Filter from './components/Filter';
 import Products from './components/Products';
 import data from "./data.json";
@@ -11,10 +12,33 @@ class App extends React.Component {
      super();
      this.state = {
        products: data.products,
+       cartItems: [],
        size: "",
        sort: "",
      }
   }
+
+  removeFromCart = (product) =>{
+    const cartItems = this.state.cartItems.slice();
+    this.setState({ cartItems: cartItems.filter(x=>x.id !== product.id),
+    });
+  }
+
+  addToCart = (product) => {
+     const cartItems = this.state.cartItems.slice();
+     let alreadyInCart = false;
+     cartItems.forEach(item =>{
+       if(item.id === product.id)
+       {
+         item.count++;
+         alreadyInCart = true;
+       }
+     });
+     if(!alreadyInCart){
+       cartItems.push({...product, count: 1});
+     }
+     this.setState({cartItems})
+  };
 
   sortProducts = (event) =>{
     //imp
@@ -64,10 +88,14 @@ class App extends React.Component {
                filterProducts={this.filterProducts}
                sortProducts={this.sortProducts}
                />
-                <Products products={this.state.products}/>
+                <Products
+                 products={this.state.products}
+                 addToCart={this.addToCart}/>
              </div>
              <div className="sidebar">
-                Cart Items
+                <Cart 
+                cartItems={this.state.cartItems}
+                removeFromCart={this.removeFromCart}/>
              </div>
           </div>
         </main>
@@ -77,7 +105,6 @@ class App extends React.Component {
       </div>
     );
   }
-  
 }
 
 export default App;
--- src/components/Products.js
+++ src/components/Products.js
@@ -18,7 +18,9 @@ export default class Products extends Component {
                               
                               <div className="product-price">
                               <div>{formatCurrency(product.price)}</div> 
-                                <button className="button primary">
+                                <button
+                                onClick={()=> this.props.addToCart(product)}
+                                 className="button primary">
                                   Add To Cart
                               </button>
                               </div>
